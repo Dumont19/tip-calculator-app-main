4bd1bfa89130d7ebeac04fc64b75c2bb8a559a7a
dumont19 <dumont.matheus19@gmail.com>
2023-11-18 10:53:44 -0300

    js almost done

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ const tipCalculator = () => {
   })
 
   const validateInput = () => {
-    if (inputPeople.value === "" || inputPeople.value === 0) {
+    if (inputPeople.value === "" || inputPeople.value === '0') {
       errorMessage.style.display = "block"
       inputPeople.classList.add("error")
     } else {
@@ -29,20 +29,22 @@ const tipCalculator = () => {
   }
 
   const calculate = () => {
-    tipValue.forEach((btn) => {
-      btn.addEventListener("click", (event) => {
+    tipValue.forEach(btn => {
+      btn.addEventListener("click", ({ target }) => {
         validateInput()
         const bill = Number(inputBill.value)
         const tip = Number(inputTip.value)
         const people = Number(inputPeople.value)
-        const btn = event.target.value
+        const btn = target.value
         const amountValue = (bill * (btn / 100) / people).toFixed(2)
         const totalValue = ((((btn / 100) * bill) + bill) / people).toFixed(2)
 
-        if (inputBill.value === '' || inputPeople.value === '') {
+        if (inputBill.value === '' || inputPeople.value === '' || inputPeople.value === Infinity) {
           amount.textContent = `$0.00`
           total.textContent = `$0.00`
         } else {
+          reset.disabled = false
+          reset.style.opacity = '1'
           total.textContent = `$${totalValue}`
           amount.textContent = `$${amountValue}`
         }
@@ -58,6 +60,7 @@ const tipCalculator = () => {
       inputPeople.value = ""
       amount.textContent = "$0.00"
       total.textContent = "$0.00"
+      reset.style.opacity = '.25'
     })
   }
   resetInputs()
